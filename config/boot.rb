ecf5fa15ed395d6b8567ffa061437f59b311eb2f
Darren Hardy <drh@stanford.edu>
2014-03-19 14:04:58 -0700

    use upcase for ROBOT_LOG_LEVEL

--- config/boot.rb
+++ config/boot.rb
@@ -7,10 +7,9 @@ require 'logger'
 # Load the environment file based on Environment.  Default to development
 environment = ENV['ROBOT_ENVIRONMENT'] ||= 'development'
 ROBOT_LOG = Logger.new(File.join(File.dirname(__FILE__), "../log/#{environment}.log"))
-ROBOT_LOG.level = Logger::SEV_LABEL.index(ENV['ROBOT_LOG_LEVEL']) || Logger::INFO
+ROBOT_LOG.level = Logger::SEV_LABEL.index((ENV['ROBOT_LOG_LEVEL'] || 'info').upcase) || Logger::INFO
 
 env_file = File.expand_path(File.dirname(__FILE__) + "/./environments/#{environment}")
-# puts "Loading config from #{env_file}"
 require env_file
 
 require 'dor-workflow-service'
