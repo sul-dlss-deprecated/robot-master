2f8ac803049e25039d98bb4d9339854c735c9a13
Darren Hardy <drh@stanford.edu>
2014-03-19 16:49:13 -0700

    added priority classes

--- lib/robot-master.rb
+++ lib/robot-master.rb
@@ -52,15 +52,31 @@ module RobotMaster
     def perform_step(step)
       ROBOT_LOG.info("Processing #{@repository}:#{@workflow}:#{step[:name]}")
       ROBOT_LOG.debug { "depends on #{step[:prereq].join(',')}" }
-            
+      
+      # fetch pending jobs for this step from the Workflow Service. 
+      # we need to always do this to determine whether there are high priority jobs pending.
       results = Dor::WorkflowService.get_objects_for_workstep(step[:prereq], step[:name], @repository, @workflow, true, step[:limit])
       ap({:results => results})
       ROBOT_LOG.debug { "Found #{results.size} druids" }
       
-      return unless has_priority_items?(results) or priority_queue_empty?(step)
+      # search the priority queues to determine whether we need to enqueue to them
+      needs_work = false
       
+      # if we have jobs at a priority level for which the job queue is empty
+      priority_classes(results.values).each do |priority|
+        ROBOT_LOG.debug { "Checking priority queue for #{step[:name]} #{priority}..." }
+        needs_work = true if priority_queue_empty?(step[:name], priority)
+      end
+      
+      # if we have any high priority jobs at all
+      needs_work = true if has_priority_items?(results)
+      
+      ROBOT_LOG.debug { "needs_work=#{needs_work}" }
+      return unless needs_work
+      
+      # perform the mediation
       results.each do |druid, priority|
-        enqueue(step[:name], druid, priority)
+        enqueue(step[:name], druid, priority_class(priority))
         update_status_to_enqueued(step[:name], druid)
       end
     end
@@ -69,26 +85,30 @@ module RobotMaster
     
     # @return [Boolean] true if the results queue has any high priority items
     def has_priority_items?(results)
-      results.each_value.any? {|priority| priority > 0 }
+      results.each_value.any? { |priority| priority > 0 }
     end
     
+    # @param [String] step
+    # @param [Symbol] priority `:high`, `:default`, or `:low`
+    # @param [Integer] threshold The number of items below which the queue is considered empty
     # @return [Boolean] true if the Resque priority queue for the step needs some more jobs
-    def priority_queue_empty?(step)
-      # raise NotImplementedError
-      %w{high default low}.each do |priority|
-        queue = "#{ENV['ROBOT_ENVIRONMENT']}_#{@workflow}_#{step[:name]}_#{priority}"
-        # puts queue
-      end
-      true # XXX
+    #
+    # XXX: doesn't handle low-priority cases -- will never fill the low queue unless high and default full
+    def priority_queue_empty?(step, priority = :high, threshold = 100)
+      queue = "prod_#{@workflow}_#{step}_#{priority}" # XXX: needs to come from environment
+      n = Resque.size(queue)
+      ROBOT_LOG.debug { "queue size=#{n} #{queue}"}
+      (n < threshold)
     end
     
     # Adds the given item to the priority queue for this step
     #
     # @param [String] step name of the step
     # @param [String] druid
-    # @param [Integer] priority
+    # @param [Symbol] priority `:high`, `:default`, or `:low`
     def enqueue(step, druid, priority)
-      raise NotImplementedError
+      ROBOT_LOG.debug { "enqueue #{step} #{druid} #{priority}" }
+      # raise NotImplementedError # XXX
     end
     
     # Updates the status from `waiting` (implied) to `queued` in the Workflow Service
@@ -96,7 +116,8 @@ module RobotMaster
     # @param [String] step name of the step
     # @param [String] druid
     def update_status_to_enqueued(step, druid)
-      raise NotImplementedError
+      ROBOT_LOG.debug { "update_status_to_enqueued #{step} #{druid}" }
+      # raise NotImplementedError # XXX
     end
 
     # Parses the process XML to extract name and prereqs
@@ -128,6 +149,24 @@ module RobotMaster
         prereqs << prereq.text
       end
       { :name => step, :prereq => prereqs }
+    end
+    
+    # @param [Integer] priority where 0 is default, positive is high, and negative is low
+    # @return [Symbol] one of `:high`, `:default`, `:low` keys for into which class the given priority falls
+    def priority_class(priority)
+      if priority > 0
+        :high
+      elsif priority < 0
+        :low
+      else
+        :default
+      end
+    end
+    
+    # @return [Array<Symbol>] contains `:high`, `:default`, `:low` keys for into which classes the 
+    #   given priorities fall
+    def priority_classes(priorities)
+      priorities.uniq.collect {|priority| priority_class(priority) }.uniq
     end
   end
 end
