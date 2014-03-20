c5d9cd26d410d35c19bd52f7a7113bcda9f35119
Darren Hardy <drh@stanford.edu>
2014-03-20 13:19:04 -0700

    use redis:namespace

--- config/example_environment.rb
+++ config/example_environment.rb
@@ -1,2 +1,2 @@
-WORKFLOW_URL = 'http://example.com/workflow/'
-REDIS_URL = 'redis://example.com:6379/db'
+WORKFLOW_URL = 'http://127.0.0.1/workflow/'
+REDIS_URL = '127.0.0.1:6379/resque:mynamespace' # hostname:port[:db][/namespace]
