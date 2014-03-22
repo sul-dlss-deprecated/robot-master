33d8f5a212a78c496da0f30436364a36ec53f77d
Darren Hardy <drh@stanford.edu>
2014-03-21 21:32:23 -0700

    major rewrite of tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,35 +1,16 @@
-$LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+
+require 'bundler/setup'
+Bundler.require(:default, :development)
 
 require 'rubygems'
 require 'rspec'
 require 'awesome_print'
 require 'equivalent-xml'
 
+
 RSpec.configure do |config|
   
 end
-
-module Kernel
-  def require_one(*args)
-    args.each do |mod|
-      begin
-        return require(mod)
-      rescue LoadError
-      end
-    end
-    raise LoadError, "could not load any of the following -- #{args.join(', ')}"
-  end
-  
-  # Suppresses warnings within a given block.
-  def with_warnings_suppressed
-    saved_verbosity = $-v
-    $-v = nil
-    yield
-  ensure
-    $-v = saved_verbosity
-  end
-end
-
 
 Rails = Object.new unless defined? Rails
--- spec/unit/robot_master_spec.rb
+++ spec/unit/robot_master_spec.rb
@@ -1,82 +1,127 @@
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 require File.expand_path(File.dirname(__FILE__) + '/../../config/boot')
-require 'rspec'
-require 'spec_helper'
 
 describe RobotMaster do
-  before(:each) do
-    @subject = RobotMaster::Workflow.new('dor', 'accessionWF')
-  end
+  let(:priorities) {  
+    %w{critical high default low}.map(&:to_sym)
+  }
+  subject(:master) {
+    RobotMaster::Workflow.new('dor', 'accessionWF')
+  }
   
   it '#queue_name' do
-    %w{critical high default low}.each do |priority|
-      @subject.queue_name('foo-bar', priority.to_sym).should == 'dor_accessionWF_foo-bar_' + priority
+    priorities.each do |priority|
+      expect(master.queue_name('foo-bar', priority)).to eq "dor_accessionWF_foo-bar_#{priority}"
     end
-    @subject.queue_name('foo-bar', 0) == 'dor_accessionWF_foo-bar_default'
-    @subject.queue_name('dor:someWF:foo-bar', -1) == 'dor_someWF_foo-bar_low'
+    expect(master.queue_name('foo-bar', 0)).to eq 'dor_accessionWF_foo-bar_default'
+    expect(master.queue_name('dor:someWF:foo-bar', -1)).to eq 'dor_someWF_foo-bar_low'
   end
   
-  it '#priority_class' do
-    @subject.priority_class(101).should == :critical
-    @subject.priority_class(1).should == :high
-    @subject.priority_class(0).should == :default
-    @subject.priority_class(-1).should == :low
+  context '#priority_class' do
+    it 'critical' do
+      expect(master.priority_classes([101, 1000])).to eq [:critical]
+    end
+    
+    it 'high' do
+      expect(master.priority_classes([1, 10, 100])).to eq [:high]
+    end
+    
+    it 'default' do
+      expect(master.priority_class(0)).to equal :default
+    end
+    
+    it 'low' do
+      expect(master.priority_classes([-1, -10, -100, -1000])).to eq [:low]
+    end
+  
+    it 'critical and low' do
+      expect(master.priority_classes([-100, 1000, -1, 150])).to eq [:critical, :low]
+    end
   end
   
-  it '#prority_classes' do
-    @subject.priority_classes([100, -100, 1000, -1, 99, 150]).should == [:critical, :high, :low]
-    @subject.priority_classes([0, 0]).should == [:default]
+  context '#has_priority_items?' do
+    it 'false' do
+      [[0], [0, -1], [0, -100, 0]].each do |i|
+        expect(master.has_priority_items?(i)).to be false
+      end
+    end
+    it 'true' do
+      [[1], [0, 1, 0], [0, 100, 1000]].each do |i|
+        expect(master.has_priority_items?(i)).to be true
+      end
+    end
   end
   
-  it '#has_priority_items?' do
-    @subject.has_priority_items?([0]).should == false
-    @subject.has_priority_items?([0, -1]).should == false
-    @subject.has_priority_items?([1]).should == true
-    @subject.has_priority_items?([100, 0, -100]).should == true
-  end
-  
-  it '#parse_process_node -- with prereqs' do
-    doc = Nokogiri::XML('
-     <process name="remediate-object" sequence="6">
-       <label>Ensure object conforms to latest DOR standards and schemas</label>
-       <prereq>content-metadata</prereq>
-       <prereq>descriptive-metadata</prereq>
-       <prereq>technical-metadata</prereq>
-       <prereq>rights-metadata</prereq>
-      </process>')
-    @subject.parse_process_node(doc.root).should == {
-      :name => "dor:accessionWF:remediate-object",
-    :prereq => [
-         "dor:accessionWF:content-metadata",
-         "dor:accessionWF:descriptive-metadata",
-         "dor:accessionWF:technical-metadata",
-         "dor:accessionWF:rights-metadata"
-      ],
-      :skip => false
-    }
-  end
-  
-  it '#parse_process_node -- with skip queue' do
-    doc = Nokogiri::XML('
-     <process name="foo-bar" sequence="1" skip-queue="true">
-       <prereq>content-metadata</prereq>
-      </process>')
-    @subject.parse_process_node(doc.root).should == {
-      :name => "dor:accessionWF:foo-bar",
-    :prereq => [ "dor:accessionWF:content-metadata" ],
-      :skip => true
-    }
-  end
-  
-  it '#parse_process_node -- with qualified name' do
-    doc = Nokogiri::XML('
-     <process name="foo-bar" sequence="1">
-       <prereq>dor:assemblyWF:jp2-create</prereq>
-      </process>')
-    @subject.parse_process_node(doc.root).should == {
-      :name => "dor:accessionWF:foo-bar",
-    :prereq => [ "dor:assemblyWF:jp2-create" ],
-      :skip => false
-    }
-  end
+  context '#parse_process_node' do
+    it 'empty' do
+      doc = Nokogiri::XML('<process name="initiate"/>')
+      expect(master.parse_process_node(doc.root)[:name]).to eq 'dor:accessionWF:initiate'
+      expect(master.parse_process_node(doc.root)[:skip]).to be false
+    end
 
+    it 'waiting' do
+      doc = Nokogiri::XML('<process name="initiate" status="waiting"/>')
+      expect(master.parse_process_node(doc.root)[:skip]).to be false
+    end
+
+    it 'completed' do
+      doc = Nokogiri::XML('<process name="initiate" status="completed"/>')
+      expect(master.parse_process_node(doc.root)[:skip]).to be true
+    end
+
+    it 'skip-queue' do
+      doc = Nokogiri::XML('<process name="initiate" skip-queue="true"/>')
+      expect(master.parse_process_node(doc.root)[:skip]).to be true
+    end
+
+    it 'skip-queue false' do
+      doc = Nokogiri::XML('<process name="initiate" skip-queue="false"/>')
+      expect(master.parse_process_node(doc.root)[:skip]).to be false
+    end
+  
+    context "single prereq" do
+      let(:doc) {
+        Nokogiri::XML('
+         <process name="remediate-object">
+           <prereq>content-metadata</prereq>
+          </process>'
+        )
+      }
+      it 'should' do
+        expect(master.parse_process_node(doc.root)[:prereq]).to eq [
+             'dor:accessionWF:content-metadata'
+          ]
+      end
+    end
+  
+    context 'multiple prereqs' do
+      let(:doc) {
+        Nokogiri::XML('
+         <process name="remediate-object">
+           <prereq>content-metadata</prereq>
+           <prereq>descriptive-metadata</prereq>
+           <prereq>technical-metadata</prereq>
+           <prereq>rights-metadata</prereq>
+          </process>'
+        )
+      }
+      it 'should' do
+        expect(master.parse_process_node(doc.root)[:prereq]).to eq [
+             'dor:accessionWF:content-metadata',
+             'dor:accessionWF:descriptive-metadata',
+             'dor:accessionWF:technical-metadata',
+             'dor:accessionWF:rights-metadata'
+          ]
+      end
+    end
+    
+    it 'with qualified name' do
+      doc = Nokogiri::XML('
+       <process name="foo-bar">
+         <prereq>dor:assemblyWF:jp2-create</prereq>
+        </process>')
+      expect(master.parse_process_node(doc.root)[:name]).to eq 'dor:accessionWF:foo-bar'
+      expect(master.parse_process_node(doc.root)[:prereq]).to eq ['dor:assemblyWF:jp2-create']
+    end
+  end
 end
