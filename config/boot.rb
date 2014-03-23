4cf95dbf9f12a749fe1fc90d7242ab267602c981
Darren Hardy <drh@stanford.edu>
2014-03-22 23:31:28 -0700

    initialize logs after loading environment configuration

--- config/boot.rb
+++ config/boot.rb
@@ -6,11 +6,11 @@ require 'logger'
 
 # Load the environment file based on Environment.  Default to development
 environment = ENV['ROBOT_ENVIRONMENT'] ||= 'development'
-ROBOT_LOG = Logger.new(File.join(File.dirname(__FILE__), "../log/#{environment}.log"))
-ROBOT_LOG.level = Logger::SEV_LABEL.index((ENV['ROBOT_LOG_LEVEL'] || 'info').upcase) || Logger::INFO
-
 env_file = File.expand_path(File.dirname(__FILE__) + "/./environments/#{environment}")
 require env_file
+
+ROBOT_LOG = Logger.new(ENV['ROBOT_LOG'] || File.join(File.dirname(__FILE__), "../#{environment}.log"))
+ROBOT_LOG.level = Logger::SEV_LABEL.index((ENV['ROBOT_LOG_LEVEL'] || 'info').upcase) || Logger::INFO
 
 require 'dor-workflow-service'
 Dor::WorkflowService.configure(WORKFLOW_URL)
