6cb162c646686f019febe80fdf57d4244ac5f6f5
Darren Hardy <drh@stanford.edu>
2014-03-29 20:26:42 -0700

    use self.xxx for class methods

--- lib/robot-master/priority.rb
+++ lib/robot-master/priority.rb
@@ -4,67 +4,64 @@ module RobotMaster
     # all possible priority classes
     PRIORITIES = %w{critical high default low}.map(&:to_sym)
   
-    class << self
-      # Converts the given priority number into a priority class
-      # 
-      # - `:critical` when priority > 100
-      # - `:high` when 0 < priority <= 100 
-      # - `:default` when priority == 0
-      # - `:low` when priority < 0
-      #
-      # @param [Integer] priority
-      # @return [Symbol] the priority class into which the given priority falls
-      def priority_class(priority)
-        if priority > 100
-          :critical
-        elsif priority > 0 and priority <= 100
-          :high
-        elsif priority < 0
-          :low
-        else
-          :default
+    # Converts the given priority number into a priority class
+    # 
+    # - `:critical` when priority > 100
+    # - `:high` when 0 < priority <= 100 
+    # - `:default` when priority == 0
+    # - `:low` when priority < 0
+    #
+    # @param [Integer] priority
+    # @return [Symbol] the priority class into which the given priority falls
+    def self.priority_class(priority)
+      if priority > 100
+        :critical
+      elsif priority > 0 and priority <= 100
+        :high
+      elsif priority < 0
+        :low
+      else
+        :default
+      end
+    end
+
+    # @param [Array<Integer>, Array<Symbol>] priorities
+    # @return [Boolean] true if the results queue has any high 
+    #   or critical priority items
+    #
+    # @example
+    #    has_priority_items?([0, -1, -1, 0])
+    #    => false
+    #    has_priority_items?([0, 0, 1])
+    #    => true
+    #    has_priority_items?([:default, :default, :high])
+    #    => true
+    def self.has_priority_items?(priorities)
+      priorities.each.any? { |priority|
+        unless priority.is_a?(Symbol) or priority.is_a?(Numeric)
+          raise ArgumentError, "Illegal priority value #{priority}" 
         end
-      end
-  
-      # @param [Array<Integer>, Array<Symbol>] priorities
-      # @return [Boolean] true if the results queue has any high 
-      #   or critical priority items
-      #
-      # @example
-      #    has_priority_items?([0, -1, -1, 0])
-      #    => false
-      #    has_priority_items?([0, 0, 1])
-      #    => true
-      #    has_priority_items?([:default, :default, :high])
-      #    => true
-      def has_priority_items?(priorities)
-        priorities.each.any? { |priority|
-          unless priority.is_a?(Symbol) or priority.is_a?(Numeric)
-            raise ArgumentError, "Illegal priority value #{priority}" 
-          end
-          [:critical, :high].include?(
-            priority.is_a?(Numeric) ?
-              priority_class(priority.to_i) :
-              priority
-            )
-        }
-      end
-  
-      # Converts all priority numbers into the possible priority classes.
-      #
-      # @param [Array<Integer>] priorities
-      # @return [Array<Symbol>] a unique array of priority classes into 
-      #   which the given priorities fall, in order of highest priority first.
-      # @example
-      #     priority_classes([1000, 101, 100, -100, -1, 99, 150])
-      #     => [:critical, :high, :low]
-      def priority_classes(priorities)
-        # iterate in high-to-low order on unique priorities
-        priorities.uniq.sort.reverse.collect do |priority| 
-          priority_class(priority)
-        end.uniq
-      end
-      
+        [:critical, :high].include?(
+          priority.is_a?(Numeric) ?
+            priority_class(priority.to_i) :
+            priority
+          )
+      }
+    end
+
+    # Converts all priority numbers into the possible priority classes.
+    #
+    # @param [Array<Integer>] priorities
+    # @return [Array<Symbol>] a unique array of priority classes into 
+    #   which the given priorities fall, in order of highest priority first.
+    # @example
+    #     priority_classes([1000, 101, 100, -100, -1, 99, 150])
+    #     => [:critical, :high, :low]
+    def self.priority_classes(priorities)
+      # iterate in high-to-low order on unique priorities
+      priorities.uniq.sort.reverse.collect do |priority| 
+        priority_class(priority)
+      end.uniq
     end
   end
 end
--- lib/robot-master/queue.rb
+++ lib/robot-master/queue.rb
@@ -1,73 +1,71 @@
 module RobotMaster
   # Manages a workflow to enqueue jobs into a priority queue
   module Queue
-    class << self
-      # Generate the queue name from step and priority
-      # 
-      # @param [String] step fully qualified name
-      # @param [Symbol | Integer] priority
-      # @return [String] the queue name
-      # @example
-      #     queue_name('dor:assemblyWF:jp2-create')
-      #     => 'dor_assemblyWF_jp2-create_default'
-      #     queue_name('dor:assemblyWF:jp2-create', 100)
-      #     => 'dor_assemblyWF_jp2-create_high'
-      def queue_name(step, priority = :default)
-        Workflow.assert_qualified(step)
-        unless priority.is_a?(Integer) or Priority::PRIORITIES.include?(priority)
-          raise ArgumentError, "Unknown priority: #{priority}"
-        end
-        [ 
-          Workflow.parse_qualified(step),
-          priority.is_a?(Integer) ? Priority.priority_class(priority) : priority
-        ].flatten.join('_')
+    # Generate the queue name from step and priority
+    # 
+    # @param [String] step fully qualified name
+    # @param [Symbol | Integer] priority
+    # @return [String] the queue name
+    # @example
+    #     queue_name('dor:assemblyWF:jp2-create')
+    #     => 'dor_assemblyWF_jp2-create_default'
+    #     queue_name('dor:assemblyWF:jp2-create', 100)
+    #     => 'dor_assemblyWF_jp2-create_high'
+    def self.queue_name(step, priority = :default)
+      Workflow.assert_qualified(step)
+      unless priority.is_a?(Integer) or Priority::PRIORITIES.include?(priority)
+        raise ArgumentError, "Unknown priority: #{priority}"
       end
+      [ 
+        Workflow.parse_qualified(step),
+        priority.is_a?(Integer) ? Priority.priority_class(priority) : priority
+      ].flatten.join('_')
+    end
+
+    # @param [String] step a fully qualified name
+    # @param [Symbol, Integer] priority
+    # @param [Integer] threshold The number of items below which the queue is considered empty
+    # @return [Boolean] true if the queue for the step is "empty"
+    def self.queue_empty?(step, priority = :default, threshold = 100)
+      Workflow.assert_qualified(step)
+      queue = queue_name(step, priority)
+      n = Resque.size(queue)
+      ROBOT_LOG.debug { "queue size=#{n} #{queue}"}
+      (n < threshold)
+    end
+
+    # Adds the given item to the priority queue for this step
+    #
+    # Job names for the given step are converted like so:
+    #
+    # - `dor:assemblyWF:jp2-create` into `Robots::Assembly::Jp2Create`
+    # - `dor:etdSubmitWF:binder-transfer` into `Robots:EtdSubmit::BinderTransfer`
+    #
+    # @param [String] step fully qualified name
+    # @param [String] druid
+    # @param [Symbol] priority see `priority_class`
+    # @return [Hash] returns the `:queue` name and `klass` name enqueued
+    def self.enqueue(step, druid, priority)
+      Workflow.assert_qualified(step)
+      ROBOT_LOG.debug { "enqueue #{step} #{druid} #{priority}" }
   
-      # @param [String] step a fully qualified name
-      # @param [Symbol, Integer] priority
-      # @param [Integer] threshold The number of items below which the queue is considered empty
-      # @return [Boolean] true if the queue for the step is "empty"
-      def queue_empty?(step, priority = :default, threshold = 100)
-        Workflow.assert_qualified(step)
-        queue = queue_name(step, priority)
-        n = Resque.size(queue)
-        ROBOT_LOG.debug { "queue size=#{n} #{queue}"}
-        (n < threshold)
-      end
+      # generate the specific priority queue name
+      queue = queue_name(step, priority)
   
-      # Adds the given item to the priority queue for this step
-      #
-      # Job names for the given step are converted like so:
-      #
-      # - `dor:assemblyWF:jp2-create` into `Robots::Assembly::Jp2Create`
-      # - `dor:etdSubmitWF:binder-transfer` into `Robots:EtdSubmit::BinderTransfer`
-      #
-      # @param [String] step fully qualified name
-      # @param [String] druid
-      # @param [Symbol] priority see `priority_class`
-      # @return [Hash] returns the `:queue` name and `klass` name enqueued
-      def enqueue(step, druid, priority)
-        Workflow.assert_qualified(step)
-        ROBOT_LOG.debug { "enqueue #{step} #{druid} #{priority}" }
-    
-        # generate the specific priority queue name
-        queue = queue_name(step, priority)
-    
-        # generate the robot job class name
-        r, w, s = Workflow.parse_qualified(step)
-        klass = [
-          'Robots',
-          r.camelcase,
-          w.sub('WF', '').camelcase,
-          s.sub('-', '_').camelcase
-        ].join('::')
-        ROBOT_LOG.debug { "enqueue_to: #{queue} #{klass} #{druid}" }
-    
-        # perform the enqueue to Resque
-        Resque.enqueue_to(queue.to_sym, klass, druid)
-    
-        { :queue => queue, :klass => klass }
-      end
+      # generate the robot job class name
+      r, w, s = Workflow.parse_qualified(step)
+      klass = [
+        'Robots',
+        r.camelcase,
+        w.sub('WF', '').camelcase,
+        s.sub('-', '_').camelcase
+      ].join('::')
+      ROBOT_LOG.debug { "enqueue_to: #{queue} #{klass} #{druid}" }
+  
+      # perform the enqueue to Resque
+      Resque.enqueue_to(queue.to_sym, klass, druid)
+  
+      { :queue => queue, :klass => klass }
     end
   end
 end
