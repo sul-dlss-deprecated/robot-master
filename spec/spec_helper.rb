2376839e116652351e2d81bda43754f766c1f40b
Darren Hardy <drh@stanford.edu>
2014-04-01 11:48:51 -0700

    testing robot-master log goes to /dev/null by default

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,8 @@
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 
-ENV['ROBOT_ENVIRONMENT'] = 'development'
-ENV['ROBOT_LOG_LEVEL'] = 'debug'
+ENV['ROBOT_ENVIRONMENT'] ||= 'development'
+ENV['ROBOT_LOG'] ||= '/dev/null'
+ENV['ROBOT_LOG_LEVEL'] ||= 'debug'
 
 require 'simplecov'
 SimpleCov.start
