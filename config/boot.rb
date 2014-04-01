163b2492f6212a99dd864625f58565434293de66
Darren Hardy <drh@stanford.edu>
2014-04-01 11:27:12 -0700

    default ROBOT_LOG is stdout

--- config/boot.rb
+++ config/boot.rb
@@ -5,17 +5,18 @@ require 'bundler/setup'
 require 'logger'
 
 # Load the environment file based on Environment.  Default to development
-environment = ENV['ROBOT_ENVIRONMENT'] ||= 'development'
-env_file = File.expand_path(File.dirname(__FILE__) + "/./environments/#{environment}")
-require env_file
+ENV['ROBOT_ENVIRONMENT'] ||= 'development'
+require File.expand_path(File.join(File.dirname(__FILE__), 'environments', ENV['ROBOT_ENVIRONMENT']))
 
-ROBOT_LOG = Logger.new(ENV['ROBOT_LOG'] || File.join(File.dirname(__FILE__), "../#{environment}.log"))
-ROBOT_LOG.level = Logger::SEV_LABEL.index((ENV['ROBOT_LOG_LEVEL'] || 'info').upcase) || Logger::INFO
+ENV['ROBOT_LOG'] ||= 'stdout'
+ENV['ROBOT_LOG_LEVEL'] ||= 'info'
+ROBOT_LOG = Logger.new(ENV['ROBOT_LOG'].downcase == 'stdout' ? STDOUT : ENV['ROBOT_LOG'])
+ROBOT_LOG.level = Logger::SEV_LABEL.index(ENV['ROBOT_LOG_LEVEL'].upcase) || Logger::INFO
 
 require 'dor-workflow-service'
+WORKFLOW_URL ||= 'http://127.0.0.1/workflow'
 Dor::WorkflowService.configure(WORKFLOW_URL)
 
-require 'resque'
 # @see http://rubydoc.info/gems/redis/3.0.7/file/README.md
 # @see https://github.com/resque/resque
 #
@@ -26,6 +27,8 @@ require 'resque'
 #   Redis::Namespace - a namespaced redis connection that will be used as-is
 #   Redis::Distributed - a distributed redis connection that will be used as-is
 #   Hash - a redis connection hash (e.g. {:host => 'localhost', :port => 6379, :db => 0})
+require 'resque'
+REDIS_URL ||= "localhost:6379/resque:#{ENV['ROBOT_ENVIRONMENT']}"
 Resque.redis = REDIS_URL
 
 require 'active_support/core_ext' # camelcase
