9e9bd5f983d5245c8923ab68a19cfa6538947692
Darren Hardy <drh@stanford.edu>
2014-04-03 15:26:55 -0700

    fixed error Update workflow status failed for waiting->queued transition: uninitialized constant RobotMaster::Workflow::WorkflowService

--- lib/robot-master/workflow.rb
+++ lib/robot-master/workflow.rb
@@ -109,7 +109,7 @@ module RobotMaster
       r, w, s = Workflow.parse_qualified(step)
       begin
         if ENV['ROBOT_MASTER_ENABLE_UPDATE_WORKFLOW_STATUS'] == 'yes'
-          WorkflowService.update_workflow_status(r, druid, w, s, mark_status, expected: 'waiting')
+          Dor::WorkflowService.update_workflow_status(r, druid, w, s, mark_status, expected: 'waiting')
         end
       rescue => e
         ROBOT_LOG.warn("Update workflow status failed for waiting->queued transition: #{e}")
