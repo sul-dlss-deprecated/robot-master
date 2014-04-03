a541c04e74271e3b514f443e13f51ba8c1b6e2d1
Darren Hardy <drh@stanford.edu>
2014-04-03 10:45:09 -0700

    cleanup gitignore for config/environments

--- config/environments/example.rb
+++ config/environments/example.rb
@@ -1,5 +1,5 @@
 WORKFLOW_URL = 'http://127.0.0.1/workflow/'
-REDIS_URL = '127.0.0.1:6379/resque:mynamespace' # hostname:port[:db][/namespace]
+REDIS_URL = '127.0.0.1:6379/resque:development' # hostname:port[:db][/namespace]
 ENV['ROBOT_ENVIRONMENT'] ||= 'development'
 ENV['ROBOT_LOG'] ||= 'stdout'
 ENV['ROBOT_LOG_LEVEL'] ||= 'debug'
