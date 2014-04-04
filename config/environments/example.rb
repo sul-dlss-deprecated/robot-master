fa2afd1061ab3bda28fba654faf67d07ffb56203
Darren Hardy <drh@stanford.edu>
2014-04-04 09:15:11 -0700

    add RESTCLIENT_LOG

--- config/environments/example.rb
+++ config/environments/example.rb
@@ -3,3 +3,4 @@ REDIS_URL = '127.0.0.1:6379/resque:development' # hostname:port[:db][/namespace]
 ENV['ROBOT_ENVIRONMENT'] ||= 'development'
 ENV['ROBOT_LOG'] ||= 'stdout'
 ENV['ROBOT_LOG_LEVEL'] ||= 'debug'
+ENV['RESTCLIENT_LOG'] ||= 'stdout'
