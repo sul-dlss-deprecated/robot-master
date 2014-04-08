abbf11cd3d8267b0f605b76b3dfca812d552b0f7
Darren Hardy <drh@stanford.edu>
2014-04-08 15:52:01 -0700

    noop for restart

--- config/deploy.rb
+++ config/deploy.rb
@@ -43,9 +43,7 @@ namespace :deploy do
   desc 'Restart application'
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
-      execute :controller, 'stop'
-      execute :controller, 'quit'
-      execute :controller, 'boot'
+      # execute :rake, 'restart'
     end
   end
 
