aa02795182ed3bb12281a28227273df2e349f4f1
Darren Hardy <drh@stanford.edu>
2014-05-15 11:41:29 -0700

    fixes #16

--- config/environments/bluepill_development.rb
+++ config/environments/bluepill_development.rb
@@ -8,12 +8,6 @@ REPEAT_EVERY = 15 # seconds
 
 Bluepill.application 'robot-master', 
   :log_file => "#{WORKDIR}/log/bluepill.log" do |app|
-  app.process('rackup') do |process|
-    process.start_command "bundle exec rackup -p 3000"
-    process.working_dir = WORKDIR
-    process.daemonize = true
-    process.stdout = process.stderr = "#{WORKDIR}/log/rackup.log"
-  end
   
   [ENV['ROBOT_ENVIRONMENT']].each do |e|
       WORKFLOW_STEPS.each do |wf|
