104e47398679e1e9cb965ed88905c4424885c8df
Darren Hardy <drh@stanford.edu>
2014-08-19 13:25:30 -0700

    sync

--- config/environments/example.rb
+++ config/environments/example.rb
@@ -1,3 +1,16 @@
+cert_dir = File.join(File.dirname(__FILE__), "..", "certs")
+
+Dor::Config.configure do
+  solrizer.url 'http://example.com/solr/collection'
+  fedora.url 'https://example.com/fedora'
+
+  ssl do
+    cert_file File.join(cert_dir,"example.crt")
+    key_file File.join(cert_dir,"example.key")
+    key_pass ''
+  end
+end
+
 # @see https://github.com/sul-dlss/dor-workflow-service
 WORKFLOW_URL = 'http://127.0.0.1/workflow/'
 WORKFLOW_TIMEOUT = 60 # in seconds
@@ -15,7 +28,5 @@ WORKFLOW_TIMEOUT = 60 # in seconds
 REDIS_URL = '127.0.0.1:6379/resque:development' # hostname:port[:db][/namespace]
 
 ENV['ROBOT_ENVIRONMENT'] ||= 'development'
-ENV['ROBOT_LOG'] ||= 'stdout'
-ENV['ROBOT_LOG_LEVEL'] ||= 'debug'
-
-ENV['RESTCLIENT_LOG'] ||= 'stdout'
+ENV['ROBOT_MASTER_ENABLE_UPDATE_WORKFLOW_STATUS'] ||= 'yes'
+ENV['ROBOT_MASTER_CONCURRENT'] ||= '0'
