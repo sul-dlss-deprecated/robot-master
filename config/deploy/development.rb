1456175852d3d81a972aa8f642481c168c991435
Darren Hardy <drh@stanford.edu>
2016-01-14 11:32:52 -0800

    upgrade to ruby 2.1.2 and a full bundle update

--- config/deploy/development.rb
+++ config/deploy/development.rb
@@ -1,7 +1,7 @@
 set :deploy_environment, 'development'
 set :whenever_environment, fetch(:deploy_environment)
 set :default_env, robot_environment: fetch(:deploy_environment)
-# set :rvm_ruby_version, '2.1.0'
+set :rvm_ruby_version, '2.1.2'
 
 # Simple Role Syntax
 # ==================
