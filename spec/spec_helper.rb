40c89746e1809d433020b22e5653753f4d133a9a
Chris Beer <cabeer@stanford.edu>
2016-02-16 07:22:36 -0800

    Update the default ROBOT_ENVIRONMENT in spec_helper to match travis

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,6 @@
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 
-ENV['ROBOT_ENVIRONMENT'] ||= 'development'
+ENV['ROBOT_ENVIRONMENT'] ||= 'local'
 ENV['ROBOT_LOG'] ||= '/dev/null'
 ENV['ROBOT_LOG_LEVEL'] ||= 'debug'
 
