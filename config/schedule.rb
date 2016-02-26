d6d832c19c63593d7cd6c82f541a20f186eb621c
Darren Hardy <drh@stanford.edu>
2016-02-26 12:27:38 -0800

    switch crontab to use ruby 2.2.4

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,5 +1,5 @@
 set :output, '/home/lyberadmin/robot-master/current/log/crondebug.log'
 
 every :day, at: '1:36am', roles: [:app] do
-  command "BUNDLE_GEMFILE=/home/lyberadmin/robot-master/current/Gemfile /usr/local/rvm/gems/ruby-1.9.3-p484@global/bin/bundle exec /home/lyberadmin/robot-master/current/bin/robot-sweeper --environment=#{environment} --log=/home/lyberadmin/robot-master/current/log/sweeper.log"
+  command "BUNDLE_GEMFILE=/home/lyberadmin/robot-master/current/Gemfile /usr/local/rvm/gems/ruby-2.2.4@global/bin/bundle exec /home/lyberadmin/robot-master/current/bin/robot-sweeper --environment=#{environment} --log=/home/lyberadmin/robot-master/current/log/sweeper.log"
 end
