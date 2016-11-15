75e5723237b8f24c5fcd455eb16fea6ce064798a
Darren Hardy <drh@stanford.edu>
2016-11-15 15:11:40 -0800

    switch to run the sweeper 2x/day

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,5 +1,5 @@
 set :output, '/home/lyberadmin/robot-master/current/log/crondebug.log'
 
-every :day, at: '1:36am', roles: [:app] do
+every 12.hours, roles: [:app] do
   command "cd /home/lyberadmin/robot-master/current && BUNDLE_GEMFILE=Gemfile /usr/local/rvm/gems/ruby-2.2.4@global/bin/bundle exec bin/robot-sweeper --environment=#{environment} --log=log/sweeper.log"
 end
